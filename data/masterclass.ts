68253ac5defe0ccd83bab0035241a6bd45277ab4
Veeravel-S <veeravel.igt@gmail.com>
2026-08-06 19:32:06 +0530

    changes

--- data/masterclass.ts
+++ data/masterclass.ts
@@ -25,7 +25,7 @@ export const masterclass: MasterclassConfig = {
   startTime: "10:30 AM",
   endTime: "01:30 PM",
   timeZone: "IST",
-  classStartAt: "2026-09-01T10:30:00+05:30",
+  classStartAt: "2026-08-01T10:30:00+05:30",
   pageName: "consumer-protection-law-masterclass",
 
   format: "Online (Live)",
